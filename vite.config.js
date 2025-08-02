0396647c2cb98af859d7dad9fa7c28c1872281ae
Amit376-hub <kumaramitkumar6209@gmail.com>
2025-08-02 10:54:32 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/E-Commerce-website/', // 👈 exactly your repo name
   plugins: [react()],
+  base: "/E-commerce"
 })
